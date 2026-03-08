fa3fc555482124af3db2f67df26c93d443e46659
nakayoshi-ver01 <nakayoshiver01@gmail.com>
2026-03-08 22:39:49 +0900

    フィルター機能のテスト

--- problem/web/test/todo_app/spec_test.cljs
+++ problem/web/test/todo_app/spec_test.cljs
@@ -42,3 +42,60 @@
     (is (true? (m/validate req-items/update-todo-item {:content "更新後"})))
     (is (true? (m/validate req-items/update-todo-item {:done true})))
     (is (true? (m/validate req-items/update-todo-item {:content "更新後" :done false})))))
+
+;; フィルター機能のテスト
+(defn apply-filter 
+  "アイテムリストにフィルターを適用する関数
+  problem.web.src.todo_app.state.todo_items.cljs>::filtered-itemsの中身を抜き出す"
+  [items filter-type]
+  (case filter-type
+    :active (filterv (complement :done) items)
+    :done (filterv :done items)
+    items))
+
+(deftest filter-todo-items-test
+  (let [items [{:id 1 :content "完了したタスク" :done true}
+               {:id 2 :content "未完了のタスク1" :done false}
+               {:id 3 :content "未完了のタスク2" :done false}]]
+    
+    (testing "フィルター機能の動作"
+      (testing ":allフィルター - すべてのアイテムが返される"
+        (is (= 3 (count (apply-filter items :all))))
+        (is (= items (apply-filter items :all))))
+      
+      (testing ":activeフィルター - 未完了(:done=false)のアイテムだけが返される"
+        (is (= 2 (count (apply-filter items :active))))
+        (is (= [{:id 2 :content "未完了のタスク1" :done false}
+                {:id 3 :content "未完了のタスク2" :done false}]
+               (apply-filter items :active))))
+      
+      (testing ":doneフィルター - 完了(:done=true)のアイテムだけが返される"
+        (is (= 1 (count (apply-filter items :done))))
+        (is (= [{:id 1 :content "完了したタスク" :done true}]
+               (apply-filter items :done))))
+      
+      (testing "空のリストでのフィルター"
+        (is (= 0 (count (apply-filter [] :all))))
+        (is (= 0 (count (apply-filter [] :active))))
+        (is (= 0 (count (apply-filter [] :done))))))))
+
+(deftest filter-edge-cases-test
+  (testing "フィルターのエッジケース"
+    (let [items [{:id 1 :content "タスク1" :done false}]]
+      
+      (testing "単一アイテムでのフィルター"
+        (is (= 1 (count (apply-filter items :all))))
+        (is (= 1 (count (apply-filter items :active))))
+        (is (= 0 (count (apply-filter items :done)))))
+      
+      (testing "すべて完了している場合"
+        (let [all-done [{:id 1 :done true} {:id 2 :done true}]]
+          (is (= 2 (count (apply-filter all-done :all))))
+          (is (= 0 (count (apply-filter all-done :active))))
+          (is (= 2 (count (apply-filter all-done :done))))))
+      
+      (testing "すべて未完了の場合"
+        (let [all-active [{:id 1 :done false} {:id 2 :done false}]]
+          (is (= 2 (count (apply-filter all-active :all))))
+          (is (= 2 (count (apply-filter all-active :active))))
+          (is (= 0 (count (apply-filter all-active :done)))))))))
